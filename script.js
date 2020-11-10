bcb00c9d763d4bac0b1cab5ee79d839eabadd00b
redgarblagov <blagovesh.maxim@gmail.com>
2020-11-10 16:04:53 +0300

    Project files

---
+++ script.js
@@ -0,0 +1,12 @@
+function printConsole (arg1, arg2, arg3) {
+    console.log('Сообщение из функции' )
+    console.log(arg1 +3);
+    console.log(arg2 + 2);
+} // только обьявление, поэтому точку с запятой не ставим
+
+printConsole(1, 2, 3);
+
+const foo = function()
+{
+
+};
